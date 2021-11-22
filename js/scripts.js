a406c373b93fbe0773352a90495026f8d0e67064
Ruben Sousa <rubensousayt@gmail.com>
2021-11-22 22:44:48 +0000

    abc

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 */
 //
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -32,3 +32,56 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+function initNavbar() {
+  if (
+    !$firstSection.is('.parallax-section, .dark-bg, .colored-bg') &&
+    $homeSlider.length === 0
+  ) {
+    $navBar.addClass('stick');
+  }
+
+  if ($firstSection.is('.parallax-section.text-dark')) {
+    $body.addClass('light-slide');
+  }
+
+  $navBar.find('.navigation-menu>li').slice(-2).addClass('last-elements');
+
+  $('.menu-toggle, .toggle-nav').on('click', function(event) {
+    event.preventDefault();
+    if ($window.width() < 992) {
+      $(this).find('.hamburger').toggleClass('is-active');
+
+      $('#navigation').slideToggle(400);
+      $navBar.find('.cart-open').removeClass('opened');
+    }
+  });
+
+  $.merge($navBar, $asideNav).on('click', '.navigation-menu li.menu-item-has-children>a', function(e) {
+    if ($window.width() < 992) {
+      e.preventDefault();
+      $(this).parent('li').toggleClass('opened').find('.submenu:first').slideToggle();
+    }
+  });
+
+  $('#navigation .navigation-menu a[data-scroll="true"]').on('click', function() {
+    if ($window.width() < 992) {
+      $('.menu-toggle').trigger('click');
+    }
+  });
+
+  $body.scrollspy({
+    target: '#navigation',
+  });
+
+  $navBar.on('click', '.cart-open>a', function(event) {
+    if ($window.width() < 992) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      if ($('#navigation').is(':visible')) {
+        $('.menu-toggle').trigger('click');
+      }
+
+      $(this).parent('.cart-open').toggleClass('opened');
+    }
+  });
